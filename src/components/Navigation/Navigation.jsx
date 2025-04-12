f5e852c1ca8acff5264a976c2bbe5ec317255467
Nazar <lagoydanazar@gmail.com>
2025-04-12 00:42:42 -0400

    styled home page

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,28 +1,47 @@
-import Logo from '../contactsComponents/logo/Logo.jsx'
-import { NavLink } from 'react-router-dom'
-import css from './Navigation.module.css'
+import Logo from "../contactsComponents/logo/Logo.jsx";
+import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
 
 const Navigation = () => {
+  return (
+    <div className={css.header}>
+      <nav className={css.navbar}>
+        <Logo />
+        <ul className={css.navWrapper}>
+          <li className={css.navList}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `${css.link} ${isActive ? css.active : ""}`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className={css.navList}>
+            <NavLink
+              to="/contacts"
+              className={({ isActive }) =>
+                `${css.link} ${isActive ? css.active : ""}`
+              }
+            >
+              Contacts
+            </NavLink>
+          </li>
+          <li className={css.navList}>
+            <NavLink
+              to="/bridge"
+              className={({ isActive }) =>
+                `${css.link} ${isActive ? css.active : ""}`
+              }
+            >
+              Landing Page
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-    return (
-        <div className={css.header}>
-            <nav>
-                <ul className={css.navbar}>
-                    <Logo />
-                    <li className={css.navList}>
-                        <NavLink className={css.link} to='/'>Home</NavLink>
-                    </li>
-                    <li className={css.navList}>
-                        <NavLink to='/contacts' className={css.link}>Contacts</NavLink>
-                    </li>
-                    <li className={css.navList}>
-                    <NavLink to="/bridge" className={css.link}>Landing Page</NavLink>
-                    </li>
-                </ul>
-            </nav >
-        </div>
-    )
-}
-
-export default Navigation
-
+export default Navigation;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,45 +3,64 @@ import myPhoto from "../../public/myPhotos/myPhoto.jpeg";
 import styles from "./Home.module.css";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
+
 const Home = () => {
   return (
-    <div className="container">
-      <div className={styles["personal-info"]}>
-        <h1 className={styles.initial}>Hi, i am Nazariy Lahoida.</h1>
-        <p className={styles.moto}>
-          A Sydney based front-end developer passionate about building
-          accessible and user friendly websites.
-        </p>
-        <img
-          src={myPhoto}
-          width={250}
-          height={250}
-          alt="My photo"
-          className={styles.image}
-        />
-      </div>
-      <div className={styles["personal-links"]}>
-        <a
-          href="/public/myPhotos/myPhoto.jpeg"
-          download={myPhoto}
-          className={styles.myResume}
-        >
-          Download <FontAwesomeIcon icon={faDownload} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/nazariy-lahoida-a417911b8/"
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <FontAwesomeIcon icon={faLinkedin} size="xl" />
-        </a>
-        <a
-          href="https://github.com/Nazar1us"
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <FontAwesomeIcon icon={faGithub} size="xl" />
-        </a>
+    <div className={styles.homeWrapper}>
+      <div className="container">
+        <section className={styles["personal-info"]}>
+          <div className={styles["hero-info"]}>
+            <h1 className={styles.initial}>
+              HI, I AM <br /> <span>NAZARIY LAHOIDA.</span>
+            </h1>
+            <p className={styles.moto}>
+              Front-end developer passionate about building accessible and user
+              friendly websites.
+            </p>
+            <div className={styles["personal-links"]}>
+              <a
+                href="/public/myPhotos/myPhoto.jpeg"
+                download={myPhoto}
+                className={styles.myResume}
+              >
+                Download <FontAwesomeIcon icon={faDownload} size="1x" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/nazariy-lahoida-a417911b8/"
+                target="_blank"
+                referrerPolicy="no-referrer"
+              >
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  size="lg"
+                  className={styles.faLinkedin}
+                  width={50}
+                  height={50}
+                />
+              </a>
+              <a
+                href="https://github.com/Nazar1us"
+                target="_blank"
+                referrerPolicy="no-referrer"
+              >
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  size="lg"
+                  className={styles.faGithub}
+                />
+              </a>
+            </div>
+          </div>
+          <div className={styles["hero-image"]}>
+            <img
+              src={myPhoto}
+              width={350}
+              height={350}
+              alt="My photo"
+              className={styles.image}
+            />
+          </div>
+        </section>
       </div>
     </div>
   );
