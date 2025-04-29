4da04bb0fd6e052c8fbbccbcd99d943d5ce4de81
Nazar <lagoydanazar@gmail.com>
2025-04-28 21:16:23 -0400

    styled project component

--- src/components/Portfolio/ProjectCard/ProjectCard.jsx
+++ src/components/Portfolio/ProjectCard/ProjectCard.jsx
@@ -3,43 +3,39 @@ import PropTypes from "prop-types";
 
 const ProjectCard = ({ project }) => {
   return (
-    <>
-      <article className={styles.project}>
-        <div className={styles["project-image"]}>
-          <img
-            src={`/projects/${project.image}`}
-            alt={project.title}
-            width={500}
-            height={500}
-          />
-        </div>
+    <article className={styles.project}>
+      <div className={styles["project-details"]}>
+        <h3>{project.title}</h3>
+        <p>{project.description}</p>
 
-        <div className={styles["project-details"]}>
-          <h3>{project.title}</h3>
-          <p>{project.description}</p>
-
-          <div className={styles["project-info"]}></div>
-          <div className={styles["projects-links"]}>
-            <a
-              href={project.liveDemo}
-              className="live-demo"
-              target="_blank"
-              referrerPolicy="no-referrer"
-            >
-              Live Demo
-            </a>
-            <a
-              href={project.github}
-              className="github-link"
-              target="_blank"
-              referrerPolicy="no-referrer"
-            >
-              See on GitHub
-            </a>
-          </div>
+        <div className={styles["projects-links"]}>
+          <a
+            href={project.liveDemo}
+            className="live-demo"
+            target="_blank"
+            referrerPolicy="no-referrer"
+          >
+            Live Demo
+          </a>
+          <a
+            href={project.github}
+            className="github-link"
+            target="_blank"
+            referrerPolicy="no-referrer"
+          >
+            See on GitHub
+          </a>
         </div>
-      </article>
-    </>
+      </div>
+      <div className={styles["project-image"]}>
+        <img
+          src={`/projects/${project.image}`}
+          alt={project.title}
+          width={500}
+          height={500}
+        />
+      </div>
+    </article>
   );
 };
 
